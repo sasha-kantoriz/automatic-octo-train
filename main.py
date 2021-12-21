827efe38de7ffb1ffaf406085e0e2df863155f2c
sasha-kantoriz <no.money.no.honey5151@gmail.com>
2021-12-21 10:34:49 +0200

    Add handling for bogus input

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ def memory_outage_handler(f):
             return f(*args, **kwargs)
         except MemoryError as e:
             print('Input File Too Large. Aborting...')
-            exit(1)
+            exit(2)
     return catch_memory_outage
-
 
 
 @memory_outage_handler
 def _read_input(file_path) -> list:
-    numbers = list()
     with open(file_path) as fp:
-        nums = fp.read().split()
-        for num in nums:
-            numbers.append(int(num))
-    return numbers
+        try:
+            nums = list(map(int, fp.read().split()))
+        except ValueError as e:
+            print('Wrong input type. Aborting...')
+            exit(1)
+    return nums
 
 
 @memory_outage_handler
